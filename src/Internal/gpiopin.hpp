654bb11ae60ddf6a86687154360ef27e5d3c5f08
Tyler Tian <tylertian123@gmail.com>
2018-10-02 21:29:16 -0400

    Add constructor to GPIOPin class

--- src/Internal/gpiopin.hpp
+++ src/Internal/gpiopin.hpp
@@ -9,6 +9,9 @@ class GPIOPin {
 public:
 	GPIO_TypeDef *port;
 	uint16_t pin;
+
+	GPIOPin(GPIO_TypeDef *port, uint16_t pin) : port(port), pin(pin) {}
+	GPIOPin() : port(0), pin(0) {}
 	
 	void set(const bool &val);
 	bool get(void) const;
