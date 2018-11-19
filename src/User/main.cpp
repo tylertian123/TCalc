86074b105040dbfc7b80c20d0e08c5d1066c0b29
Tyler Tian <tylertian123@gmail.com>
2018-11-18 22:52:50 -0500

    Add key handling for all numbers, letters and space

--- src/User/main.cpp
+++ src/User/main.cpp
@@ -175,6 +175,201 @@ int main() {
 			case KEY_DELETE:
 				cursor->removeChar();
 				break;
+			/* LETTER KEYS */
+			case KEY_A:
+				cursor->addChar('A');
+				break;
+			case KEY_B:
+				cursor->addChar('B');
+				break;
+			case KEY_C:
+				cursor->addChar('C');
+				break;
+			case KEY_D:
+				cursor->addChar('D');
+				break;
+			case KEY_E:
+				cursor->addChar('E');
+				break;
+			case KEY_F:
+				cursor->addChar('F');
+				break;
+			case KEY_G:
+				cursor->addChar('G');
+				break;
+			case KEY_H:
+				cursor->addChar('H');
+				break;
+			case KEY_I:
+				cursor->addChar('I');
+				break;
+			case KEY_J:
+				cursor->addChar('J');
+				break;
+			case KEY_K:
+				cursor->addChar('K');
+				break;
+			case KEY_L:
+				cursor->addChar('L');
+				break;
+			case KEY_M:
+				cursor->addChar('M');
+				break;
+			case KEY_N:
+				cursor->addChar('N');
+				break;
+			case KEY_O:
+				cursor->addChar('O');
+				break;
+			case KEY_P:
+				cursor->addChar('P');
+				break;
+			case KEY_Q:
+				cursor->addChar('Q');
+				break;
+			case KEY_R:
+				cursor->addChar('R');
+				break;
+			case KEY_S:
+				cursor->addChar('S');
+				break;
+			case KEY_T:
+				cursor->addChar('T');
+				break;
+			case KEY_U:
+				cursor->addChar('U');
+				break;
+			case KEY_V:
+				cursor->addChar('V');
+				break;
+			case KEY_W:
+				cursor->addChar('W');
+				break;
+			case KEY_X:
+				cursor->addChar('X');
+				break;
+			case KEY_Y:
+				cursor->addChar('Y');
+				break;
+			case KEY_Z:
+				cursor->addChar('Z');
+				break;
+			case KEY_LCA:
+				cursor->addChar('a');
+				break;
+			case KEY_LCB:
+				cursor->addChar('b');
+				break;
+			case KEY_LCC:
+				cursor->addChar('c');
+				break;
+			case KEY_LCD:
+				cursor->addChar('d');
+				break;
+			case KEY_LCE:
+				cursor->addChar('e');
+				break;
+			case KEY_LCF:
+				cursor->addChar('f');
+				break;
+			case KEY_LCG:
+				cursor->addChar('g');
+				break;
+			case KEY_LCH:
+				cursor->addChar('h');
+				break;
+			case KEY_LCI:
+				cursor->addChar('i');
+				break;
+			case KEY_LCJ:
+				cursor->addChar('j');
+				break;
+			case KEY_LCK:
+				cursor->addChar('k');
+				break;
+			case KEY_LCL:
+				cursor->addChar('l');
+				break;
+			case KEY_LCM:
+				cursor->addChar('m');
+				break;
+			case KEY_LCN:
+				cursor->addChar('n');
+				break;
+			case KEY_LCO:
+				cursor->addChar('o');
+				break;
+			case KEY_LCP:
+				cursor->addChar('p');
+				break;
+			case KEY_LCQ:
+				cursor->addChar('q');
+				break;
+			case KEY_LCR:
+				cursor->addChar('r');
+				break;
+			case KEY_LCS:
+				cursor->addChar('s');
+				break;
+			case KEY_LCT:
+				cursor->addChar('t');
+				break;
+			case KEY_LCU:
+				cursor->addChar('u');
+				break;
+			case KEY_LCV:
+				cursor->addChar('v');
+				break;
+			case KEY_LCW:
+				cursor->addChar('w');
+				break;
+			case KEY_LCX:
+				cursor->addChar('x');
+				break;
+			case KEY_LCY:
+				cursor->addChar('y');
+				break;
+			case KEY_LCZ:
+				cursor->addChar('z');
+				break;
+			/* OTHER KEYS WITH NO SPECIAL HANDLING */
+			case KEY_0:
+				cursor->addChar('0');
+				break;
+			case KEY_1:
+				cursor->addChar('1');
+				break;
+			case KEY_2:
+				cursor->addChar('2');
+				break;
+			case KEY_3:
+				cursor->addChar('3');
+				break;
+			case KEY_4:
+				cursor->addChar('4');
+				break;
+			case KEY_5:
+				cursor->addChar('5');
+				break;
+			case KEY_6:
+				cursor->addChar('6');
+				break;
+			case KEY_7:
+				cursor->addChar('7');
+				break;
+			case KEY_8:
+				cursor->addChar('8');
+				break;
+			case KEY_9:
+				cursor->addChar('9');
+				break;
+			case KEY_SPACE:
+				cursor->addChar(' ');
+				break;
+			case KEY_COMMA:
+				cursor->addChar(',');
+				break;
+			
 			default: break;
 			}
 			
