bbbef930d321b6342141288294fefdfd5a869bea
Tyler Tian <tylertian123@gmail.com>
2018-11-25 23:58:10 -0500

    Add drawInvertedString method to lcd

--- src/Hardware/lcd12864.hpp
+++ src/Hardware/lcd12864.hpp
@@ -89,6 +89,7 @@ namespace lcd {
 		void drawImage(int16_t, int16_t, const LCD12864Image&, bool invert = false);
 		void drawLine(int16_t, int16_t, int16_t, int16_t);
         void drawString(int16_t, int16_t, const char*);
+        void drawInvertedString(int16_t, int16_t, const char*);
         void fill(int16_t, int16_t, uint16_t, uint16_t);
 	
 	protected:
