c72da14489e6978efe54cdc88fbc8aea5810d62d
Tyler Tian <tylertian123@gmail.com>
2018-11-26 19:58:26 -0500

    Add basic impl of eval::Number, eval::Operator, eval::LeftBracket and eval::RightBracket

--- src/Internal/eval.cpp
+++ src/Internal/eval.cpp
@@ -2,5 +2,18 @@
 
 namespace eval {
 
-    
+    /******************** Operator ********************/
+    uint8_t Operator::getPrecedence() {
+        switch(type) {
+        case Type::EXPONENT:
+            return 0;
+        case Type::MULTIPLY:
+        case Type::DIVIDE:
+            return 1;
+        case Type::PLUS:
+        case Type::MINUS:
+            return 2;
+        default: return 0xFF;
+        }
+    }
 }
--- src/Internal/eval.hpp
+++ src/Internal/eval.hpp
@@ -16,6 +16,44 @@ namespace eval {
     public:
         virtual TokenType getType() = 0;
     };
+
+    class Number : public Token {
+    public:
+        double value;
+
+        virtual TokenType getType() override {
+            return TokenType::NUMBER;
+        }
+    };
+
+    class Operator : public Token {
+    public:
+
+        enum class Type {
+            PLUS, MINUS, MULTIPLY, DIVIDE, EXPONENT,
+        };
+
+        Type type;
+
+        uint8_t getPrecedence();
+
+        virtual TokenType getType() override {
+            return TokenType::NUMBER;
+        }
+    };
+
+    class LeftBracket : public Token {
+    public:
+        virtual TokenType getType() override {
+            return TokenType::L_BRACKET;
+        }
+    };
+    class RightBracket : public Token {
+    public:
+        virtual TokenType getType() override {
+            return TokenType::R_BRACKET;
+        }
+    };
 }
 
 #endif
