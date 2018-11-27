8693f34b6418b0c15c88c7d55f1f9d724fe7a81f
Tyler Tian <tylertian123@gmail.com>
2018-11-26 22:56:38 -0500

    Fix bugs and add resizing in Deque<T>::push

--- src/Internal/deque.hpp
+++ src/Internal/deque.hpp
@@ -10,36 +10,44 @@ public:
     Deque(uint16_t capacity) : len(0), start(0), maxLen(capacity) {
         contents = (T*) malloc(sizeof(T) * capacity);
     }
+    Deque() : len(0), start(0), maxLen(0), contents((T*) malloc(0)) {}
     ~Deque() {
         free(contents);
+    }
+
+    bool increaseSize() {
+        uint16_t oldMaxLen = maxLen;
+        //Default: allocate only what's needed
+        maxLen = len + 1;
+        void *tmp = realloc(contents, sizeof(T) * maxLen);
+        if(!tmp) {
+            maxLen = oldMaxLen;
+            return false;
+        }
+        //Adjust the queue to accommodate for the change
+        contents = (T*) tmp;
+        uint16_t i = maxLen - 1;
+        uint16_t index = (start + len) % maxLen;
+        while(i != index) {
+            if(i == maxLen - 1) {
+                contents[i] = contents[0];
+            }
+            else {
+                contents[i] = contents[i + 1];
+            }
+            ++i;
+            if(i >= maxLen) {
+                i = 0;
+            }
+        }
+        return true;
     }
 
     bool enqueue(T elem) {
         //Check for overflow
         if(len + 1 > maxLen) {
-            uint16_t oldMaxLen = maxLen;
-            //Default: allocate only what's needed
-            maxLen = len + 1;
-            void *tmp = realloc(contents, sizeof(T) * maxLen);
-            if(!tmp) {
-                maxLen = oldMaxLen;
+            if(!increaseSize()) {
                 return false;
-            }
-            //Adjust the queue to accommodate for the change
-            contents = (T*) tmp;
-            uint16_t i = maxLen - 1;
-            uint16_t index = (start + len) % maxLen;
-            while(i != index) {
-                if(i == maxLen - 1) {
-                    contents[i] = contents[0];
-                }
-                else {
-                    contents[i] = contents[i + 1];
-                }
-                ++i;
-                if(i >= maxLen) {
-                    i = 0;
-                }
             }
         }
         uint16_t index = (start + len) % maxLen;
@@ -47,7 +55,14 @@ public:
         ++len;
         return true;
     }
-    void push(T elem) {
+    bool push(T elem) {
+        //Check for overflow
+        if(len + 1 > maxLen) {
+            if(!increaseSize()) {
+                return false;
+            }
+        }
+
         if(start > 0) {
             --start;
         }
@@ -55,6 +70,7 @@ public:
             start = maxLen - 1;
         }
         contents[start] = elem;
+        ++len;
     }
     T dequeue() {
         T &temp = contents[start];
@@ -70,6 +86,9 @@ public:
     }
     T peek() {
         return contents[start];
+    }
+    bool isEmpty() {
+        return !(len > 0);
     }
 
 protected:
