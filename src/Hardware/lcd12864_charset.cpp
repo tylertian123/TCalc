05a822961f7fd1f69606fd6e213c635ed40543db
Tyler Tian <tylertian123@gmail.com>
2018-12-08 12:09:52 -0500

    Remove unnecessary functions in lcd12864_charset and change arrays to pointers

--- src/Hardware/lcd12864_charset.cpp
+++ src/Hardware/lcd12864_charset.cpp
@@ -10,13 +10,6 @@ namespace lcd {
 		return c - LCD_CHARSET_LOWBOUND;
 	}
 	const Img& getChar(char c) {
-		return CHAR_ASCII[asciiToIndex(c)];
-	}
-	const Img& getNumber(uint8_t n) {
-		//If the digit requested is outside the limits, return CHAR_UNKNOWN
-		if(n >= 10) {
-			return CHAR_ASCII[0x7F - LCD_CHARSET_LOWBOUND];
-		}
-		return CHAR_NUMBERS[n];
+		return *CHAR_ASCII[asciiToIndex(c)];
 	}
 }
