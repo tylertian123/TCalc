b11a4044a4c33eac90dcd5dd628189075a24d037
Tyler Tian <tylertian123@gmail.com>
2019-07-03 21:46:55 -0400

    Improve enum_key tool
    Improve enum_key script by adding include guard generation and allow
    empty lines.

--- src/Tools/enum_key.py
+++ src/Tools/enum_key.py
@@ -3,14 +3,25 @@ from sys import argv
 numstart = 0x0000
 keys = open(argv[1], "r")
 
+include_guard = None
+
 for key in keys:
     key = key[:-1]
+    if key == "":
+        print()
+        continue
     if key.startswith('EK_RESTART_NUM'):
         n = key.split()[1]
         if n.startswith('0x') or n.startswith('0X'):
             numstart = int(n[2:], base=16)
         else:
             numstart = int(n)
+        continue
+    elif key.startswith('EK_INCLUDE_GUARD'):
+        include_guard = key.split()[1]
+        print(f"#ifndef {include_guard}")
+        print(f"#define {include_guard}")
+        print()
         continue
 
     eq = key.find('=')
@@ -21,5 +32,8 @@ for key in keys:
     else:
         print(f"#define KEY_{key.split('=')[0].upper()}{' ' * (10 - len(key.split('=')[0]))}{key.split('=')[1]}")
 
+if include_guard != None:
+    print()
+    print("#endif")
     
 keys.close()
