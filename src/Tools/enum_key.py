ecba46cd43c14532027c1a08a85c7030f417a498
Tyler Tian <tylertian123@gmail.com>
2019-07-10 14:10:55 -0400

    Change key enums
    Change key enums from sequential to randomized in the range from
    0x0000-0xFFFF inclusive. This means that now two bit flips (which would
    not be detected by the parity checking) is now less likely to generate
    another valid key combo.

--- src/Tools/enum_key.py
+++ src/Tools/enum_key.py
@@ -1,9 +1,13 @@
 from sys import argv
+from random import randrange
 
 numstart = 0x0000
 keys = open(argv[1], "r")
 
+used_nums = set()
+
 include_guard = None
+randnum = False
 
 for key in keys:
     key = key[:-1]
@@ -16,6 +20,7 @@ for key in keys:
             numstart = int(n[2:], base=16)
         else:
             numstart = int(n)
+        randnum = False
         continue
     elif key.startswith('EK_INCLUDE_GUARD'):
         include_guard = key.split()[1]
@@ -23,14 +28,31 @@ for key in keys:
         print(f"#define {include_guard}")
         print()
         continue
+    elif key.startswith('EK_RANDOM_NUM'):
+        randnum = True
+        continue
 
     eq = key.find('=')
-    
-    if eq == -1:
-        print(f"#define KEY_{key.upper()}{' ' * (10 - len(key))}0x{'0' * (6 - len(hex(numstart)))}{hex(numstart).upper()[2:]}")
-        numstart += 1
+    num = 0
+    if eq != -1:
+        num = int(key.split('=')[1])
     else:
-        print(f"#define KEY_{key.split('=')[0].upper()}{' ' * (10 - len(key.split('=')[0]))}{key.split('=')[1]}")
+        if not randnum:
+            num = numstart
+            numstart += 1
+            while num in used_nums:
+                num = numstart
+                numstart += 1
+        else:
+            num = randrange(0, 0x10000)
+            while num in used_nums:
+                num = randrange(0, 0x10000)
+    
+    used_nums.add(num)
+
+    name = key.upper() if eq == -1 else key.split('=')[0].upper()
+    
+    print(f"#define KEY_{name}{' ' * (10 - len(name))}0x{'0' * (6 - len(hex(num)))}{hex(num).upper()[2:]}")
 
 if include_guard != None:
     print()
