f7ff62d6cc1401ec91df6e829f938c045881188c
Tyler Tian <tylertian123@gmail.com>
2019-07-22 22:18:42 -0400

    Add volume and pressure units
    Add volume and pressure units to the conversion table.

--- src/Internal/unitconv.cpp
+++ src/Internal/unitconv.cpp
@@ -71,6 +71,56 @@ namespace eval {
     };
     constexpr uint16_t UNIT_AREA_LEN = sizeof(UNIT_AREA) / sizeof(Unit);
 
+    /* 
+     * Volume Units 
+     * Base Unit: m^3
+     */
+    const Unit UNIT_VOLUME[] = {
+        // Cubic Kilometer
+        { "km3", 1e9 },
+        // Cubic Meter
+        { "m3", 1 },
+        // Cubic Decimeter
+        { "dm3", 0.001 },
+        // Cubic Centimeter
+        { "cm3", 1e-6 },
+        // Cubic Millimeter
+        { "mm3", 1e-9 },
+        // Cubic Micrometer
+        { LCD_STR_MU "m3", 1e-18 },
+        // Cubic Nanometer
+        { "nm3", 1e-27 },
+        // Liter
+        { "L", 0.001 },
+        // Milliliter
+        { "mL", 1e-6 },
+
+        // Cubic Inch
+        { "in3", 0.00001638706 },
+        // Cubic Foot
+        { "ft3", 0.02831684659 },
+        // Cubic Yard
+        { "yd3", 0.76455485798 },
+        // Cubic Mile
+        { "mi3", 4168181825.44 },
+        // Ounce
+        { "oz", 0.00002957352 },
+        // US Gallon
+        { "USgal", 0.00378541178 },
+        // UK Gallon
+        { "UKgal", 0.00454609188 },
+        // Gallon (Imperial)
+        { "gal", 0.00454609188 },
+
+        // Cup (Metric)
+        { "cup", 0.00025 },
+        // Tablespoon (Metric)
+        { "tbsp", 1.5e-5 },
+        // Teaspoon (Metric)
+        { "tsp", 5e-6 },
+    };
+    constexpr uint16_t UNIT_VOLUME_LEN = sizeof(UNIT_VOLUME) / sizeof(Unit);
+
     /*
      * Mass Units
      * Base Unit: kg
@@ -94,19 +144,44 @@ namespace eval {
         // Pound
         { "lb", 0.453592 },
     };
-
     constexpr uint16_t UNIT_MASS_LEN = sizeof(UNIT_MASS) / sizeof(Unit);
+
+    /*
+     * Pressure Units
+     * Base Unit: Pa
+     */
+    const Unit UNIT_PRESSURE[] = {
+        // Kilopascal
+        { "kPa", 1000 },
+        // Pascal
+        { "Pa", 1 },
+        // Bar
+        { "bar", 1e5 },
+
+        // Atmosphere
+        { "atm", 101325 },
+        // Millimeter of Mercury
+        { "mmHg", 133.3224 },
+        
+        // Pounds per Square Inch
+        { "psi", 6894.75729 },
+    };
+    constexpr uint16_t UNIT_PRESSURE_LEN = sizeof(UNIT_PRESSURE) / sizeof(Unit);
 
     const Unit * const UNIT_TYPES[] = {
         UNIT_DISTANCE,
         UNIT_AREA,
+        UNIT_VOLUME,
         UNIT_MASS,
+        UNIT_PRESSURE,
     };
 
     const uint16_t UNIT_LENS[] = {
         UNIT_DISTANCE_LEN,
         UNIT_AREA_LEN,
+        UNIT_VOLUME_LEN,
         UNIT_MASS_LEN,
+        UNIT_PRESSURE_LEN,
     };
 
     constexpr uint16_t UNIT_TYPES_LEN = sizeof(UNIT_TYPES) / sizeof(Unit*);
