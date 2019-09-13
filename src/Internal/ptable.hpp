a7d3f8b58cd2a4a2a4397459b7c6fbdac0940fc9
Tyler Tian <tylertian123@gmail.com>
2019-09-13 10:04:06 -0400

    Add functions for working with the periodic table
    Add some functions to the pt namespace.

--- src/Internal/ptable.hpp
+++ src/Internal/ptable.hpp
@@ -16,14 +16,44 @@ namespace pt {
         uint8_t group;
     };
 
+    struct Location {
+        uint8_t x;
+        uint8_t y;
+    };
+
     extern const Element PERIOD_1_ELEMENTS[];
     extern const Element PERIOD_2_ELEMENTS[];
+    extern const Element PERIOD_3_ELEMENTS[];
     extern const Element PERIOD_4_ELEMENTS[];
     extern const Element PERIOD_5_ELEMENTS[];
     extern const Element PERIOD_6_ELEMENTS[];
     extern const Element PERIOD_7_ELEMENTS[];
     extern const Element LANTHANIDES[];
     extern const Element ACTINIDES[];
+
+    enum Period : uint8_t {
+        PERIOD_1 = 0,
+        PERIOD_2 = 1,
+        PERIOD_3 = 2,
+        PERIOD_4 = 3,
+        PERIOD_5 = 4,
+        PERIOD_6 = 5,
+        PERIOD_7 = 6,
+        PERIOD_EMPTY = 7,
+        PERIOD_LANTHANIDES = 8,
+        PERIOD_ACTINIDES = 9,
+    };
+
+    extern const Element * const ELEMENTS[];
+    extern const uint8_t ELEMENTS_LENGTHS[];
+    extern const uint8_t PERIOD_LENGTHS[];
+    extern const uint8_t PERIOD_COUNT;
+
+    const Element* elemWithLocation(const Location &location);
+    const Element* rightOf(Location &locationIO);
+    const Element* leftOf(Location &locationIO);
+    const Element* above(Location &locationIO);
+    const Element* below(Location &locationIO);
 }
 
 #endif
