11d608ebe8937b59f7e0a3dff6cfa7467ccb6219
Tyler Tian <tylertian123@gmail.com>
2019-12-13 09:55:35 -0500

    Fix stuff
    Fix broken build stuff with more hacks.

--- stm/dbg/build.py
+++ stm/dbg/build.py
@@ -16,8 +16,16 @@ extra_actions = [
         env.VerboseAction(blob_action, "Creating debug blob."),
         env.VerboseAction(" ".join([
             "$OBJCOPY",
+            "--change-section-lma",
+            "._user_heap_stack=0x32000000",
+            "--change-section-lma",
+            ".bss=0x30000000",
+            "${BUILD_DIR}/${PROGNAME}.elf"
+        ]), "Hacking the ELF around"),
+        env.VerboseAction(" ".join([
+            "$OBJCOPY",
             "--set-section-flags",
-            ".fw_dbg=ALLOC,CONTENTS,READONLY,DATA",
+            ".fw_dbg=ALLOC,LOAD,CONTENTS,READONLY,DATA",
             "--update-section",
             ".fw_dbg=${BUILD_DIR}/${PROGNAME}.dbgblob",
             "${BUILD_DIR}/${PROGNAME}.elf"
